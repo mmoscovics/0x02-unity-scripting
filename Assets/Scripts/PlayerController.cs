88e87046002d8606c9a4c0015256f16b767a6a72
Mitchell <mmoscovics@yahoo.com>
2020-12-23 23:15:21 -0500

    [Create] player script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody rb;
+    float moveH;
+    float moveV;
+    public float speed = 10;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        moveH = Input.GetAxis("Horizontal");
+        moveV = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3 (moveH, 0.0f, moveV);
+
+        rb.AddForce(movement * speed);
+    }
+}
